fb6f12f556a8c5a7d0827ee6381cdcbd6c3db558
Heungsub Lee <sub@subl.ee>
2017-06-02 16:03:15 +0900

    Update module name and metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from distutils.core import setup
-from distutils.core import Extension
 
-setup(name='jenkins',
+setup(name='jenkins-cffi',
       version='1.0.2',
       description="Python ctypes wrapper around Bob Jenkins' hash functions.",
-      author="John Evans",
-      author_email="john@milo.com",
-      url="http://github.com/lgastako/jenkins/",
-      py_modules=['jenkins'],
-      ext_modules=[Extension('lookup3', ['lookup3.c'])],
+      author="Heungsub Lee",
+      author_email="sub@subl.ee",
+      url="https://github.com/what-studio/jenkins-cffi/",
+      py_modules=['jenkins_cffi'],
       )
